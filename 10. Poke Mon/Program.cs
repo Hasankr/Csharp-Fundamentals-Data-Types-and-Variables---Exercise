3c39c94108b16e4a1b524f0139d091660a09037f
hasankr <hasankr@abv.bg>
2022-06-02 16:55:59 +0300

    10 added

---
+++ 10. Poke Mon/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _10._Poke_Mon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int power = int.Parse(Console.ReadLine());
+            int distance = int.Parse(Console.ReadLine());
+            int exhaustion = int.Parse(Console.ReadLine());
+            int startingPower = power;
+            int countPokedTargets = 0;
+            while (power >= distance)
+            {
+                power -= distance;
+                countPokedTargets++;
+                if (startingPower * 0.5 == power && exhaustion > 0)
+                {
+                    power /= exhaustion;
+                }
+            }
+            Console.WriteLine(power);
+            Console.WriteLine(countPokedTargets);
+
+        }
+    }
+}

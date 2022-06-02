3466e3774c8fb9a7a15d7ee11bbebcd3770dc161
hasankr <hasankr@abv.bg>
2022-06-02 16:27:28 +0300

    9 added

---
+++ 09. Spice Must Flow/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _09._Spice_Must_Flow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CONSUME_BY_WORKERS = 26;
+            const int MINIMAL_SPICES_TO_GATHER = 100;
+            const int DAILY_DECREASE = 10;
+
+            int countOfSpices = int.Parse(Console.ReadLine());
+            int totalConsumed = 0;
+            int daysCounter = 0;
+            while (countOfSpices >= MINIMAL_SPICES_TO_GATHER)
+            {
+                totalConsumed += countOfSpices - CONSUME_BY_WORKERS;
+                countOfSpices -= DAILY_DECREASE;
+                daysCounter++;
+                if (countOfSpices < MINIMAL_SPICES_TO_GATHER)
+                {
+                    totalConsumed -= CONSUME_BY_WORKERS;
+                }
+            }
+            Console.WriteLine(daysCounter);
+            Console.WriteLine(totalConsumed);
+        }
+    }
+}

db5485129f548f05e3293d57390713cba521b0c9
hasankr <hasankr@abv.bg>
2022-06-02 16:05:07 +0300

    7 and 8 Added

---
+++ 07. Water Overflow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int CAPACITY = 255;
+            int n = int.Parse(Console.ReadLine());
+            int totalQuantity = CAPACITY;
+
+            
+            for (int i = 0; i < n; i++)
+            {
+                int currliters = int.Parse(Console.ReadLine());
+                
+                if (totalQuantity - currliters >= 0)
+                {
+                 
+                    totalQuantity -= currliters;
+   
+                }
+                else
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                }
+                
+            }
+            int totalfilledquantity = CAPACITY - totalQuantity;
+            Console.WriteLine(totalfilledquantity);
+        }
+    }
+}
---
+++ 08. Beer Kegs/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _08._Beer_Kegs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double biggestKeg = double.MinValue;
+            string biggestKegName = "";
+            
+            for (int i = 0; i < n; i++)
+            {
+                string model = Console.ReadLine();
+                float radius = float.Parse(Console.ReadLine());
+                int height = int.Parse(Console.ReadLine());
+                double volume = Math.PI * Math.Pow(radius, 2) * height;
+                if (volume > biggestKeg)
+                {
+                    biggestKeg = volume;
+                    biggestKegName = model;
+                }
+            }
+            Console.WriteLine(biggestKegName);
+          
+
+        }
+    }
+}
